88080ff03d4349ce055f08b508a524f8ccd61247
Roma Slobodeniuk <romsl1991@gmail.com>
2020-02-16 03:57:43 +0200

    implemented dynamic customer cars rendering

---
+++ app/code/Roma/Test/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            getCustomerCars: 'Roma_Test/js/get-customer-cars'
+        }
+    }
+};
---
+++ app/code/Roma/Test/view/frontend/web/js/get-customer-cars.js
@@ -0,0 +1,93 @@
+/**
+ * @api
+ */
+define([
+    'jquery',
+    'mage/storage',
+    'mage/url',
+    'mage/template',
+    'text!Roma_Test/template/customer-cars.html',
+    'domReady!'
+], function ($, storage, url, mageTemplate, customerCarsTemplate) {
+    'use strict';
+
+    $.widget('roma.getCustomerCars', {
+        options: {
+            serviceUrl: 'rest/all/V1/test/cars/service/:userId',
+            userId: 0,
+            userName: 'User\'s',
+            carsTemplate: customerCarsTemplate,
+            container: '#cars-container',
+        },
+
+        _create: function () {
+            var self = this;
+            console.log('UserId: "' + this.options.userId + '"');
+            console.log('AjaxUrl: "' + this.getAjaxUrl(this.options.serviceUrl, this.options.userId) + '"');
+            $(this.element).on('click', function (event) {
+                event.preventDefault();
+                if (self.options.userId > 0) {
+                    self.renderCustomerCars($(this));
+                } else {
+                    self.renderNoCarsAnswer();
+                }
+            });
+        },
+
+        renderCustomerCars: function(element) {
+            var self = this;
+            var userId = this.options.userId;
+            var fullUrl = this.getAjaxUrl(this.options.serviceUrl, userId);
+            storage.get(
+                fullUrl, false
+            ).done(function (response) {
+                console.log(response);
+                var container = $(document).find(self.options.container);
+                var template = self.options.carsTemplate;
+                var options = {
+                    cars: response,
+                    hrefMore: element.attr('href'),
+                    userName: self.options.userName
+                };
+                var htmlToInsert = mageTemplate(template, options);
+                container.hide();
+                container.empty();
+                container.html(htmlToInsert);
+                container.animate({
+                    opacity: 1,
+                    width: "show"
+                }, 250, function() {
+                    container.show();
+                });
+                self.initClickOnCloseButton(container);
+            }).fail(function (response) {
+                console.log(response);
+            });
+        },
+
+        initClickOnCloseButton: function(container)
+        {
+            container.find('.close').off('click').on('click', function (event) {
+                event.preventDefault();
+                container.animate({
+                    opacity: 0.25,
+                    width: "hide"
+                }, 500, function() {
+                    container.hide();
+                });
+            });
+        },
+
+        renderNoCarsAnswer: function() {
+
+        },
+
+        getAjaxUrl: function (serviceUrl, userId) {
+            var ajaxUrl = serviceUrl.replace(":userId", userId);
+            url.setBaseUrl(BASE_URL);
+            return url.build(ajaxUrl)
+        }
+    });
+
+    return $.roma.getCustomerCars;
+});
